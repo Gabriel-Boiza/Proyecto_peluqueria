a67e3fa1efde57c085d334f0fafa84bf48310dbc
sergicruga <sergicruga@gmail.com>
2024-10-23 22:36:10 +0200

    creado ecss

--- src/main/java/com/example/peluqueria_3/HelloApplication.java
+++ src/main/java/com/example/peluqueria_3/HelloApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws com.example.peluqueria_3.IOException, IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Vistas/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 700);
         stage.setTitle("Login");
