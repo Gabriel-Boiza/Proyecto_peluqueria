fb47346c99bc66f4c39311ef2c35cbeccac1b2c3
Sergicruga <sergicruga@gmail.com>
2024-10-24 13:40:08 +0200

    css y pagina de agenda

--- src/main/java/com/example/peluqueria_3/HelloApplication.java
+++ src/main/java/com/example/peluqueria_3/HelloApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws com.example.peluqueria_3.IOException, IOException {
+    public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Vistas/login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 700);
         stage.setTitle("Login");
