bb735079f68f3cea4920714d59f7d16104f6f6e7
Josep-Oriol <joseporiol.ferrufino@inslapineda.cat>
2025-01-16 10:57:32 +0100

    Facturacion Vista

---
+++ src/main/java/com/example/peluqueria_3/Controllers/FacturacionController.java
@@ -0,0 +1,29 @@
+package com.example.peluqueria_3.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+
+public class FacturacionController {
+
+    @FXML private Label nombreTrabajador;
+    @FXML private Button volver;
+
+    @FXML private Tab abril;
+    @FXML private Tab agosto;
+    @FXML private Tab diciembre;
+    @FXML private Tab enero;
+    @FXML private Tab febrero;
+    @FXML private Tab julio;
+    @FXML private Tab junio;
+    @FXML private Tab marzo;
+    @FXML private Tab mayo;
+    @FXML private Tab noviembre;
+    @FXML private Tab octubre;
+    @FXML private Tab septiembre;
+
+    public void initialize(){
+
+    }
+}
